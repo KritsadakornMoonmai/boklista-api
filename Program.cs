443d5bd0340fb1632bb2b47c5d6aefb3c1dbb9f4
KritsadakornMoonmai <kritsadakorn.moonmai@outlook.com>
2026-05-20 01:02:48 +0200

    Add dockerfile. Update Program.cs for deployment with database

--- Program.cs
+++ Program.cs
@@ -27,11 +27,28 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<BookListDbContext>(options =>
-    options.UseNpgsql(connectionString));
+var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+if (connUrl != null)
+{
+    var uri = new Uri(connUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    var connString = $"Host={uri.Host};Port={uri.Port};" +
+                     $"Username={userInfo[0]};Password={userInfo[1]};" +
+                     $"Database={uri.LocalPath.TrimStart('/')};" +
+                     $"SSL Mode=Require;Trust Server Certificate=true";
+    builder.Services.AddDbContext<BookListDbContext>(opt =>
+        opt.UseNpgsql(connString));
+}
+else
+{
+    builder.Services.AddDbContext<BookListDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
+
+
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -53,6 +70,13 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IQuoteService, QuoteService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider
+        .GetRequiredService<BookListDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
